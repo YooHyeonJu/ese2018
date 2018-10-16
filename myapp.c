c987e883a273f00c45c23f0b63b34c32477274b2
Hyeonju Yoo <yhj8056@naver.com>
2018-10-16 01:01:58 -0700

    added postprocessing

--- myapp.c
+++ myapp.c
@@ -6,11 +6,16 @@ void preprocess()
 	printf("do some preprocessing\n");
 }
 
+void postprocess()
+{
+	printf("do some postprocessing\n");
+}
+
 int main(void)
 {
 	preprocess();
 	foo1();
 	foo2();
-
+	postprocess();
 	return 0;
 }
